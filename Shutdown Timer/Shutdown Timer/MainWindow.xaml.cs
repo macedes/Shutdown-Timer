a38af80da1f0a356fa6a609b0e762efc42fcca16
macedes <macedes@gmail.com>
2013-11-09 09:13:51 +0100

    new Design

--- Shutdown Timer/Shutdown Timer/MainWindow.xaml.cs
+++ Shutdown Timer/Shutdown Timer/MainWindow.xaml.cs
@@ -34,16 +34,36 @@ namespace Shutdown_Timer
         {
             if (timerstatus == false)
             {
-                
-                Double selectedComboBoxValue = Convert.ToDouble(ComboBoxMinutenPicker.SelectedValue);
 
-                Double IntervallInSeconds = TimeSpan.FromMinutes(selectedComboBoxValue).TotalMilliseconds;
+                Double selectedValue=0;
+
+                if (radio30.IsChecked == true)
+                {
+                    selectedValue = 30;
+                }
+                else if (radio60.IsChecked == true)
+                {
+                    selectedValue = 60;
+                }
+                else if (radio90.IsChecked == true)
+                {
+                    selectedValue = 90;
+                }
+                else
+                {
+                    selectedValue = 0;
+                    MessageBox.Show("Fehler");
+                }
+                
+                Double IntervallInSeconds = TimeSpan.FromMinutes(selectedValue).TotalMilliseconds;
                 ShutdownTimer.Elapsed += new ElapsedEventHandler(RunEvent);
                 ShutdownTimer.Interval = IntervallInSeconds;
                 ShutdownTimer.Enabled = true;
 
+                // Progress();
+
                 DateTime endDate = DateTime.Now;
-                endDate = endDate.AddMinutes(selectedComboBoxValue);
+                endDate = endDate.AddMinutes(selectedValue);
 
                 TimeLabel.Content = endDate.ToString();
                 TimerButton.Content = "Timer stoppen";
@@ -58,7 +78,6 @@ namespace Shutdown_Timer
 
             }
          }
-
 
         public void RunEvent(object source, ElapsedEventArgs e)
         {
@@ -77,8 +96,38 @@ namespace Shutdown_Timer
             ShutdownTimer.Enabled = false;
         }
 
-        private void ComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        //Create a Delegate that matches the Signature of the ProgressBar's SetValue method
+        private delegate void UpdateProgressBarDelegate(System.Windows.DependencyProperty dp, Object value);
+        
+        private void Progress()
         {
+            //Configure the ProgressBar
+            ProgressBar1.Minimum = 0;
+            ProgressBar1.Maximum = short.MaxValue;
+            ProgressBar1.Value = 0;
+
+            //Stores the value of the ProgressBar
+            double value = 0;
+
+            //Create a new instance of our ProgressBar Delegate that points
+            //  to the ProgressBar's SetValue method.
+            UpdateProgressBarDelegate updatePbDelegate = new UpdateProgressBarDelegate(ProgressBar1.SetValue);
+
+            //Tight Loop:  Loop until the ProgressBar.Value reaches the max
+            do
+            {
+                value += 1;
+
+                /*Update the Value of the ProgressBar:
+                  1)  Pass the "updatePbDelegate" delegate that points to the ProgressBar1.SetValue method
+                  2)  Set the DispatcherPriority to "Background"
+                  3)  Pass an Object() Array containing the property to update (ProgressBar.ValueProperty) and the new value */
+                Dispatcher.Invoke(updatePbDelegate,
+                    System.Windows.Threading.DispatcherPriority.Background,
+                    new object[] { ProgressBar.ValueProperty, value });
+
+            }
+            while (ProgressBar1.Value != ProgressBar1.Maximum);
 
         }
     }
